f973fe0be12da9f67d52927c741a58a21aa069a3
nyg159 <nyg159@gmail.com>
2024-11-26 09:03:10 +0900

    기초 세팅

---
+++ src/main/java/com/wjgy/UdiMoim/UdiMoimApplication.java
@@ -0,0 +1,13 @@
+package com.wjgy.UdiMoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdiMoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdiMoimApplication.class, args);
+	}
+
+}
